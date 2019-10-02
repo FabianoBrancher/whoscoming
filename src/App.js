dd65870247ebfa7336142721078695c02e061081
Fabiano Brancher <fabiano.brancher@hotmail.com>
2019-10-02 13:28:57 -0300

    Testing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import firebase from './config/firebase';
+
+const useDatabase = endpoint => {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const ref = firebase.database().ref(endpoint);
+
+    ref.on('value', snapshot => {
+      setData(snapshot.val());
+    });
+
+    // limpa a conexão com o firebase
+    return () => {
+      ref.off();
+    };
+  }, [endpoint]);
+  return { ...data, endpoint };
+};
+
+const useDatabasePush = endpoint => {
+  const [status, setStatus] = useState('');
+  const save = data => {
+    const ref = firebase.database().ref(endpoint);
+    ref.push(data, err => {
+      if (err) {
+        setStatus('Error');
+      } else {
+        setStatus('Success');
+      }
+    });
+  };
+  return [status, save];
+};
+
+const Comments = ({ visible }) => {
+  const endpoint = visible ? 'test' : 'test/a';
+  const data = useDatabase(endpoint);
+
+  return <pre>{JSON.stringify(data)}</pre>;
+};
+
+const A = () => {
+  const data = useDatabase('test/a');
+  return <pre>{JSON.stringify(data)}</pre>;
+};
+
 function App() {
+  const [visible, toggle] = useState(true);
+  const [status, save] = useDatabasePush('test');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button
+        type="button"
+        onClick={() => {
+          toggle(!visible);
+          save({ valor: 1, b: 2 });
+        }}
+      >
+        Toggle
+      </button>
+      Status: {status}
+      <Comments visible={visible} />
+      <A />
     </div>
   );
 }
---
+++ src/config/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBbXzM4p9CT7EXQix7Sehb1kAmUbWetHpE',
+  authDomain: 'whoscoming-app.firebaseapp.com',
+  databaseURL: 'https://whoscoming-app.firebaseio.com',
+  projectId: 'whoscoming-app',
+  storageBucket: '',
+  messagingSenderId: '880137926397',
+  appId: '1:880137926397:web:8b0503692e080c2d027094'
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
