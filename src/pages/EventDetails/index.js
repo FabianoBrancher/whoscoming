76f1740d819623d6abf00e3b4a07e97dfbb78735
Fabiano <fabiano.brancher@hotmail.com>
2019-10-29 08:57:58 -0300

    Testing CSV to JSON library

--- src/pages/EventDetails/index.js
+++ src/pages/EventDetails/index.js
@@ -5,6 +5,8 @@ import moment from 'moment';
 
 import Fuse from 'fuse.js';
 
+import * as csv from 'csvtojson';
+
 import {
   Layout,
   Row,
@@ -17,7 +19,9 @@ import {
   Dropdown,
   Menu,
   Button,
-  Modal
+  Modal,
+  Upload,
+  message
 } from 'antd';
 
 import Header from '../../components/Header';
@@ -70,6 +74,34 @@ export default function EventDetails() {
   };
 
   const fuse = new Fuse(guests, filterOptions);
+
+  const props = {
+    name: 'file',
+    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+    headers: {
+      authorization: 'authorization-text'
+    },
+    onChange(info) {
+      if (info.file.status !== 'uploading') {
+        const reader = new FileReader();
+        reader.onload = e => {
+          csv()
+            .fromString(e.target.result)
+            .then(res => console.log('Arquivo transformado para CSV', res));
+        };
+        reader.readAsText(info.file.originFileObj);
+
+        // csv()
+        //   .fromString(file)
+        //   .then(res => console.log(res));
+      }
+      if (info.file.status === 'done') {
+        message.success(`${info.file.name} file uploaded successfully`);
+      } else if (info.file.status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    }
+  };
 
   function getTitle(opt) {
     switch (opt) {
@@ -368,7 +400,12 @@ export default function EventDetails() {
                 >
                   Adicionar convidado
                 </ButtonAddGuests>
-
+                <Upload {...props}>
+                  <Button>
+                    <Icon type="upload" /> Click to Upload
+                  </Button>
+                </Upload>
+                ,
                 <Input
                   size="large"
                   placeholder="Pesquisar por nome do convidado"
